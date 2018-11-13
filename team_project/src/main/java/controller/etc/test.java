16ff8a16a6c16424d3aab4f439e4376fdfb4e32c
user <user@DESKTOP-JSJP2ND>
2018-11-13 20:22:54 +0900

    이재원 - test.java 수정

--- team_project/src/main/java/controller/etc/test.java
+++ team_project/src/main/java/controller/etc/test.java
@@ -1,7 +1,12 @@
 package controller.etc;
 
 public class test {
+	public int add(int a, int b) {
+		return a + b;
+	}
 public static void main(String[] args) {
-	System.out.println("hi");
+	test a = new test();
+	System.out.println(a.add(10, 5));
+
 	}
 }
